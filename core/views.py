8747e9cff7f4952724b2c487728f6a4be6a36844
Diego Castro <diegorbcastro@gmail.com>
2025-07-08 12:10:56 -0400

    change today 8.7.25

--- core/views.py
+++ core/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 
 class HomePageView(TemplateView):
      template_name = "core/home.html"
-     def get_context_data(self, **kwargs):
-          context = super().get_context_data(**kwargs)
-          context['titulo'] = 'Bienvenido a la pagina de la web musical'
-          return context
+    
 
+class ContactPageView(TemplateView):
+     template_name = "core/contact.html"
+     
 
